afe53f9801a11b06acd0339fcfa28fd84867ac16
Vitaliy Normatov <vnormatov@gmail.com>
2017-08-27 19:45:51 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroDetailComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
