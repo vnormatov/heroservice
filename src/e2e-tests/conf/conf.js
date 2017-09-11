6a3d33a1ed215a96c1e04a75170726d4cd5388cd
normatovv <normatovv@edi.su>
2017-09-11 18:00:20 +0300

    added allure results

--- src/e2e-tests/conf/conf.js
+++ src/e2e-tests/conf/conf.js
@@ -15,10 +15,25 @@ exports.config = {
 
     baseUrl: 'http://localhost:3000',
     framework: 'jasmine2',
+  onPrepare: function () {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+  },
 
     jasmineNodeOpts: {
         showColors: true,
         defaultTimeoutInterval: 30000
-    }
+    },
 
+  beforeLaunch: function() {
+  console.log('beforeLaunch');
+  },
 };
